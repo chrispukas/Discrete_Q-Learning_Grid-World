28c26bd97209d2fb035b71ffdf528d873f4119a6
Christopher Pukas <cp864@cam.ac.uk>
2025-08-25 13:09:21 +0100

    added dependencies

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env list | grep -q "^$ENVIRONMENT_NAME\s" || conda create --name $ENVIRONMENT_NAME python=3.12 -y
 conda activate $ENVIRONMENT_NAME
 
-if [ -f "requirements.txt" ]; then
-    conda install -c conda-forge --file requirements.txt -y
+if [ -f "environment.yml" ]; then
+    conda env update --file environment.yml --prune -y
 fi
 
 # Install local package
